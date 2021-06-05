2d2bbdee82e0fbd5d8f2ff23e8ab811d5d7b1ce4
AO Industries, Inc <support@aoindustries.com>
2021-06-05 02:14:05 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/pragmatickm/contact/model/Address.java
+++ src/main/java/com/pragmatickm/contact/model/Address.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-contact-model - Contacts nested within SemanticCMS pages and elements.
- * Copyright (C) 2013, 2014, 2015, 2016, 2019, 2020  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2019, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,8 +22,8 @@
  */
 package com.pragmatickm.contact.model;
 
-import com.aoindustries.lang.NullArgumentException;
-import static com.aoindustries.lang.Strings.nullIfEmpty;
+import com.aoapps.lang.NullArgumentException;
+import static com.aoapps.lang.Strings.nullIfEmpty;
 import java.util.Objects;
 
 public class Address {
